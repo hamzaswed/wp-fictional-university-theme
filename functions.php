6114d0ea44112f8b38db2e7bd63d86922b38c529
hamzaswed <hamzaswed66@gmail.com>
2023-07-14 13:00:53 +0300

    add and customize the blog posts

--- functions.php
+++ functions.php
@@ -1,21 +1,25 @@
-<?php 
+<?php
 
 // load theme files
-function firsttheme_setup() {
-
+function add_style_and_script_files()
+{
   // load style files
-  
-  wp_enqueue_style( 'theme-reset', get_template_directory_uri() . '/build/index.css');
-  
-  wp_enqueue_style( 'theme-google-font', "https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i");
-  
-  wp_enqueue_style( 'theme-font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-  
-  wp_enqueue_style( 'theme-style', get_template_directory_uri() . '/build/style-index.css' );
-  
+  wp_enqueue_style('theme-reset', get_template_directory_uri() . '/build/index.css');
+
+  wp_enqueue_style('theme-google-font', "https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i");
+
+  wp_enqueue_style('theme-font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+
+  wp_enqueue_style('theme-style', get_template_directory_uri() . '/build/style-index.css');
+
   // load script files
-
-  wp_enqueue_script( 'theme-script', get_template_directory_uri() . '/build/index.js', array('jquery'), '1.0.0', true);
-
+  wp_enqueue_script('theme-script', get_template_directory_uri() . '/build/index.js', array('jquery'), '1.0.0', true);
 }
-add_action( 'wp_enqueue_scripts', 'firsttheme_setup' );
+add_action('wp_enqueue_scripts', 'add_style_and_script_files');
+
+// add theme feature
+function theme_feature()
+{
+  add_theme_support('title-tag');
+}
+add_action('after_setup_theme', 'theme_feature');
--- page.php
+++ page.php
@@ -3,16 +3,19 @@
 get_header();
 
 
-if(have_posts()) {
-  
-  while(have_posts()) { 
-    
+if (have_posts()) {
+
+  while (have_posts()) {
+
     the_post(); ?>
 
     <div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>)"></div>
+      <div class="page-banner__bg-image"
+        style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>)"></div>
       <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title"><?php the_title(); ?></h1>
+        <h1 class="page-banner__title">
+          <?php the_title(); ?>
+        </h1>
         <div class="page-banner__intro">
           <p>DON'T FORGET TO FIX ME LETTER 🎈</p>
         </div>
@@ -21,50 +24,58 @@ if(have_posts()) {
     <div class="container container--narrow page-section">
 
       <?php
-        $current_page_id = get_the_ID();
-        $parent_page_id = wp_get_post_parent_id($current_page_id);
+      $current_page_id = get_the_ID();
+      $parent_page_id = wp_get_post_parent_id($current_page_id);
       ?>
 
-      <?php if($parent_page_id): ?>
-        
+      <?php if ($parent_page_id): ?>
+
         <div class="metabox metabox--position-up metabox--with-home-link">
           <p>
-            <a 
-            class="metabox__blog-home-link" 
-            href="<?php echo get_permalink($parent_page_id) ?>"
-            >
-              <i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($parent_page_id) ?> 
-            </a> 
-            <span class="metabox__main"><?php the_title() ?></span>
+            <a class="metabox__blog-home-link" href="<?php echo get_permalink($parent_page_id) ?>">
+              <i class="fa fa-home" aria-hidden="true"></i> Back to
+              <?php echo get_the_title($parent_page_id) ?>
+            </a>
+            <span class="metabox__main">
+              <?php the_title() ?>
+            </span>
           </p>
         </div>
-      
+
       <?php endif; ?>
 
 
-      <?php 
+      <?php
 
-        $arguments = get_pages([
-          'post_type' => 'page',
-          'post_status' => 'publish'
-        ]);
+      $children_of = $parent_page_id;
 
-        $children = get_page_children($current_page_id, $arguments);
+      if (!$parent_page_id) {
+        $children_of = $current_page_id;
+      }
+
+      $arguments = get_pages([
+        'post_type' => 'page',
+        'post_status' => 'publish'
+      ]);
+
+      $children = get_page_children($children_of, $arguments);
 
       ?>
 
-      <?php if(!empty($children)): ?>
+      <?php if (!empty($children)): ?>
 
         <div class="page-links">
-          <h2 class="page-links__title"><a href="#"><?php the_title() ?></a></h2>
+          <h2 class="page-links__title"><a href="<?php echo get_the_permalink($children_of) ?>">
+              <?php echo get_the_title($children_of) ?>
+            </a></h2>
 
           <ul class="min-list">
 
-            <?php foreach($children as $child): ?>
+            <?php foreach ($children as $child): ?>
 
               <?php
-                $page_title = $child->post_title;
-                $page_link = get_permalink($child->ID);
+              $page_title = $child->post_title;
+              $page_link = get_permalink($child->ID);
               ?>
 
               <li class="current_page_item">
@@ -78,7 +89,7 @@ if(have_posts()) {
           </ul>
 
         </div>
-      
+
       <?php endif ?>
 
 
